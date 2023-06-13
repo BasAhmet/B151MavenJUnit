a2eb2a796b678aa61ec00b4aa67f70eb01e2fe73
Ahmet Baş <ahba8184@gmail.com>
2023-06-14 02:10:47 +0300

    Selenium 11. gün windowHandle

---
+++ src/test/java/techproed/day11_Iframe_WindowHandle/C02_Iframe.java
@@ -0,0 +1,34 @@
+package techproed.day11_Iframe_WindowHandle;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import techproed.Utilities.TestBase;
+
+public class C02_Iframe extends TestBase {
+    @Test
+    public void iframe() {
+        //https://the-internet.herokuapp.com/iframe sayfasına gidiniz
+        driver.get("https://the-internet.herokuapp.com/iframe");
+
+        //Kalın yazının "Editor" kelimesini içerdiğini doğrulayınız
+        String text01 = driver.findElement(By.xpath("//h3")).getText();
+        Assert.assertTrue(text01.contains("Editor"));
+
+        //Textbox içindeki yazıyı siliniz.
+        driver.switchTo().frame(0);
+        WebElement textBox = driver.findElement(By.xpath("//p"));
+        bekle(2);
+        textBox.clear();
+        bekle(2);
+
+        //Sildiğiniz yazı yerine "Bu textbox iFrame içinde yer almaktadır" yazınız.
+        textBox.sendKeys("Bu textbox iFrame içinde yer almaktadır");
+        bekle(2);
+
+        //Sayfadaki "Elemental Selenium" yazısının görünür olduğunu doğrulayınız.
+        driver.switchTo().defaultContent();
+        Assert.assertTrue(driver.findElement(By.xpath("//*[@id='page-footer']//a")).isDisplayed());
+    }
+}
---
+++ src/test/java/techproed/day11_Iframe_WindowHandle/C03_WindowsHandle.java
@@ -0,0 +1,43 @@
+package techproed.day11_Iframe_WindowHandle;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WindowType;
+import techproed.Utilities.TestBase;
+
+import java.util.Set;
+
+public class C03_WindowsHandle extends TestBase {
+    @Test
+    public void windowHandle() {
+        //Window 1'de https://www.techproeducation.com adresine gidiniz
+        driver.get("https://www.techproeducation.com");
+
+        //Başlığın "Techpro Education | Online It Courses & Bootcamps" olduğunu doğrulayın
+        Assert.assertEquals(driver.getTitle(),"Techpro Education | Online It Courses & Bootcamps");
+        String techproWindowHandle = driver.getWindowHandle();
+
+        //Window 2'de https://www.youtube.com sayfasını açınız:
+        driver.switchTo().newWindow(WindowType.TAB);
+        driver.get("https://www.youtube.com");
+        String youtubeWindowHandle = driver.getWindowHandle();
+
+        //Window 3'te https://www.linkedin.com sayfasını açınız:
+        driver.switchTo().newWindow(WindowType.TAB);
+        driver.get("https://www.linkedin.com");
+        String linkdenWindowHandle = driver.getWindowHandle();
+        bekle(2);
+
+        //techproeducation sayfasına geçiniz:
+        driver.switchTo().window(techproWindowHandle);
+        bekle(2);
+
+        //youtube sayfasına geçiniz:
+        driver.switchTo().window(youtubeWindowHandle);
+        bekle(2);
+
+        //linkedIn sayfasına geçiniz:
+        driver.switchTo().window(linkdenWindowHandle);
+        bekle(2);
+    }
+}
